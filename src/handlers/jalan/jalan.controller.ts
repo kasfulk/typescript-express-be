56b5fef0aceb6fb80d0065bae9f8c4ece8813337
Kasjful Kurniawan <61037397+kasfulk@users.noreply.github.com>
2021-08-10 23:15:22 +0800

    Create jalan.controller.ts

---
+++ src/handlers/jalan/jalan.controller.ts
@@ -0,0 +1,37 @@
+import { Request, Response, Router } from 'express';
+import pool from '@services/db';
+// import { JalanTypes } from './jalan.types';
+
+export default class JalanController {
+    public path = '/jalan';
+
+    public router: Router = Router();
+
+    constructor() {
+      this.initializeRoutes();
+    }
+
+    public initializeRoutes(): void {
+      this.router.get(this.path, this.getAll);
+      this.router.get(`${this.path}/:id`, this.getId);
+    }
+
+    getAll= async (req: Request, res: Response): Promise<void> => {
+      const [rows, fields] = await pool.execute('SELECT * FROM jalan');
+
+      res.send({
+        fields:fields.map(field => field.name),
+        data: rows,
+      });
+    }
+
+    getId = async (req: Request, res: Response): Promise<void> => {
+      const { id } = req.params;
+      const [rows, fields] = await pool.execute(`SELECT * FROM jalan WHERE id_jalan = ?`,[id]);
+      
+      res.send({
+        fields:fields.map(field => field.name),
+        data: rows,
+      });
+    }
+}
