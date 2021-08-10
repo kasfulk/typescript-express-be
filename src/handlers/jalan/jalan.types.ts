0ee24ee5d6d4df821abd2e04d7860cedf601824b
Kasjful Kurniawan <61037397+kasfulk@users.noreply.github.com>
2021-08-10 23:15:56 +0800

    Create jalan.types.ts

---
+++ src/handlers/jalan/jalan.types.ts
@@ -0,0 +1,12 @@
+export interface JalanTypes {
+    id_jalan : number;
+    nama_jalan_utama: string;
+    lebar_jalan : number;
+    panjang_jalan_km : number;
+    kelurahan_yang_dilalui: string;
+    kecamatan_yang_dilalui: string;
+    prosentase_aspal : number;
+    prosentase_krikil : number;
+    prosentase_rigid : number;
+    prosentase_tanah : number;
+}
